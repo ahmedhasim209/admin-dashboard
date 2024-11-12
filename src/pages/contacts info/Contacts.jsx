04994325eaeeb6619387ecb4fbc73b81974bdb3d
Ahmmed Hashim <ahmedhasim209>
2024-11-12 21:34:39 +0200

    Create & Finish Contact info page

--- src/pages/contacts info/Contacts.jsx
+++ src/pages/contacts info/Contacts.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { rows, columns } from "./data";
 
 export default function Contacts() {
-  return <div>Contacts page</div>;
+  return (
+    <Box sx={{ mx: "auto", alignItems: "center", height: 579, width: "90%" }}>
+      <DataGrid
+        slots={{
+          toolbar: GridToolbar,
+        }}
+        rows={rows}
+        // @ts-ignore
+        columns={columns}
+      />
+    </Box>
+  );
 }
