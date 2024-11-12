e9aa2f5aa565950517e633fb3bb4f87231858366
Ahmmed Hashim <ahmedhasim209>
2024-11-12 03:23:57 +0200

    Finish all Routers for all pages of Dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,34 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import React from "react";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Team from "./pages/team/Team";
+import Contacts from "./pages/contacts info/Contacts";
+import Invoices from "./pages/invoices/Invoices";
+import Form from "./pages/form/Form";
+import Calendar from "./pages/calendar/Calendar";
+import Faq from "./pages/faq/Faq";
+import Bar from "./pages/barChart/Bar";
+import Pie from "./pages/pieChart/Pie";
+import Line from "./pages/lineChart/Line";
+import Geography from "./pages/geography/Geography";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Dashboard />} />
+      <Route path="team" element={<Team />} />
+      <Route path="contacts" element={<Contacts />} />
+      <Route path="invoices" element={<Invoices />} />
+      <Route path="form" element={<Form />} />
+      <Route path="calendar" element={<Calendar />} />
+      <Route path="faq" element={<Faq />} />
+      <Route path="bar" element={<Bar />} />
+      <Route path="pie" element={<Pie />} />
+      <Route path="line" element={<Line />} />
+      <Route path="geography" element={<Geography />} />
+    </Route>
+  )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
