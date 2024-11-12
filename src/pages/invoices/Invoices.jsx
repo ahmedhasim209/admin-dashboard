b60bd4fc4319b4c75b50b43b7b8bcd9cc8188239
Ahmmed Hashim <ahmedhasim209>
2024-11-12 21:53:19 +0200

    Create & Finish Invoices page

--- src/pages/invoices/Invoices.jsx
+++ src/pages/invoices/Invoices.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { rows, columns } from "./data";
 
 export default function Invoices() {
-  return <div>Invoices page</div>;
+  return (
+    <Box sx={{ mx: "auto", alignItems: "center", height: 579, width: "90%" }}>
+      <DataGrid
+        checkboxSelection
+        slots={{
+          toolbar: GridToolbar,
+        }}
+        rows={rows}
+        // @ts-ignore
+        columns={columns}
+      />
+    </Box>
+  );
 }
