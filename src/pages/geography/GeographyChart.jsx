9d2e537857ed274229d4aeb4b802456013972285
Ahmmed Hashim <ahmedhasim209>
2024-12-02 04:07:02 +0200

    Fix Geo header issue

--- src/pages/geography/GeographyChart.jsx
+++ src/pages/geography/GeographyChart.jsx
@@ -1,5 +1,12 @@
+import { Box } from "@mui/material";
 import Geography from "./Geography";
+import Header from "../../components/Header";
 
 export default function GeographyChart() {
-  return <Geography />;
+  return (
+    <Box>
+      <Header Title="Geography" subTitle="Simple Geography Chart" />
+      <Geography />
+    </Box>
+  );
 }
